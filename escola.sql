f6058b3bd5288ddfea859ab62613d36f2f8842b6
Angela Amaral <angela.hawerroth@gmail.com>
2023-02-12 20:17:37 +0900

    Create a BD escola and a table aluno

---
+++ escola.sql
@@ -0,0 +1,22 @@
+-- Database: escola
+
+-- DROP DATABASE IF EXISTS escola;
+
+CREATE DATABASE escola;
+
+create table aluno(
+	id_nome SERIAL PRIMARY KEY,
+	nome VARCHAR(25) not NULL,
+	e_mail VARCHAR(50) not NULL,
+	endereco VARCHAR not NULL
+);
+
+
+Insert INTO aluno (nome, e_mail, endereco) VALUES
+  ('João Alves', 'joao.alves@gmail.com', 'Ruas das flores, 34'),
+  ('Roberto Garcia', 'roberto.garcia@gmail.com', 'Rua das fontes, 321'),
+  ('Dario Rubens', 'dario.rubens@gmail.com', 'Rua dos enamorados, 87'),
+  ('Julia Lopes', 'julia.lopes@gmail.com', 'Rua graciosa, 125'),
+  ('Rebeca Giglio', 'rebeca.giglio@gmail.com', 'Rua Souza Soares, 543');
+
+ SELECT * FROM aluno;
